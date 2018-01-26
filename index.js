703e2ed2db3e2730b6ff1f0c46fcf6165be7f566
Robin Kock <robin@kock-hamburg.de>
2018-01-26 18:11:01 +0100

    port and ip

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
 const app = express();
 
 app.use(express.static("www"));
 app.use("/api", require("./routers/api"));
 
-app.listen(8080);
+app.listen(port, ip);
