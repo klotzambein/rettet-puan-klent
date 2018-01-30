4a6d3dd5809162b265b0a4bb6ba2a3fe90df02ac
Robin Kock <robin@kock-hamburg.de>
2018-01-30 14:44:26 +0100

    update redirect

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
 
 app.use("/api", require("./routers/api")(mongo));
-app.get("/", (req, res, next) => { res.redirect("rettet-puan-klent.de"); });
+app.get("/", (req, res, next) => { res.redirect("http://rettet-puan-klent.de/"); });
 app.get("/test", (req, res, next) => { res.send("working"); });
 
 // port and ip
